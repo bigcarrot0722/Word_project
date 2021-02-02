1f1a3c1935c4ad0bbc46f75c89609526eba28b0f
bigcarrot0722 <bigcarrot0722@naver.com>
2021-02-02 21:41:09 +0900

    second commit (Don't download first commit!)

---
+++ app/src/main/java/org/techtown/word_first/MainActivity.kt
@@ -0,0 +1,59 @@
+package org.techtown.word_first
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import org.techtown.word_first.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(){
+
+    //멤버 변수를 선언함
+    private lateinit var searchFragment : SearchFragment
+    private lateinit var studyFragment : StudyFragment
+    private lateinit var wordFragment : WordFragment
+    private lateinit var binding: ActivityMainBinding
+
+    companion object{
+
+        const val TAG: String = "로그"
+    }
+    //메모리에 올라갔을 때
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        //layout과 연결시키는 것
+        //setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "MainActivity - onCreat() called")
+
+        binding.bottomNav.setOnNavigationItemSelectedListener(onBottomNavigationSelectedListener)
+
+    }
+
+    private val onBottomNavigationSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {
+        when(it.itemId){
+            R.id.menu_search->{
+                Log.d(TAG, "MainActivity - 검색 클릭")
+                searchFragment = SearchFragment.newInstance()
+                supportFragmentManager.beginTransaction().replace(R.id.fragments_frame, searchFragment).commit()
+            }
+            R.id.menu_study->{
+                Log.d(TAG, "MainActivity - 공부 클릭")
+                studyFragment = StudyFragment.newInstance()
+                supportFragmentManager.beginTransaction().replace(R.id.fragments_frame, studyFragment).commit()
+            }
+            R.id.menu_word->{
+                Log.d(TAG, "MainActivity - 단어장 클릭")
+                wordFragment = WordFragment.newInstance()
+                supportFragmentManager.beginTransaction().replace(R.id.fragments_frame, wordFragment).commit()
+            }
+        }
+        true
+    }
+
+}
---
+++ app/src/main/java/org/techtown/word_first/SearchFragment.kt
@@ -0,0 +1,48 @@
+package org.techtown.word_first
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class SearchFragment: Fragment() {
+
+    companion object{
+        const val TAG : String = "로그"
+
+        fun newInstance() : SearchFragment{
+            return SearchFragment()
+        }
+    }
+
+    //메모리에 올라갔을 때
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "SearchFragment - onCreate() called")
+    }
+
+    //fragment를 안고 있는 액티비티에 붙었을 때
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.d(TAG, "SearchFragment - onAttach() called")
+    }
+
+    // 뷰가 생성되었을 때 화면과 연결해주는 것
+    //fragment와 레이아웃을 연결시켜주는 부분임.
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        Log.d(TAG, "SearchFragment - onCreateView() called")
+
+        val view = inflater.inflate(R.layout.fragment_search, container , false)
+
+        return view
+    }
+
+}
