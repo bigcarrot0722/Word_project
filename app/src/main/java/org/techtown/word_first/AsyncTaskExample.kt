8800c3f0275d6510040145c103fca66b82573929
bigcarrot0722 <bigcarrot0722@naver.com>
2021-02-08 21:10:38 +0900

    2021.02.08. 21.10
    파싱도 완벽하게 고침!

--- app/src/main/java/org/techtown/word_first/AsyncTaskExample.kt
+++ app/src/main/java/org/techtown/word_first/AsyncTaskExample.kt
@@ -4,36 +4,46 @@ import android.os.AsyncTask
 import android.util.Log
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
 
 class AsyncTaskExample(private var fragment: SearchFragment) : AsyncTask<String, Void, String>() {
     override fun doInBackground(vararg params: String?): String {
-        Log.d(SearchFragment.TAG,"loadApplications 실행됨(1)")
-        Log.d(SearchFragment.TAG,"loadApplications 실행됨(1)-${params[0]}")
+        Log.d(SearchFragment.TAG, "loadApplications 실행됨(1)")
+        Log.d(SearchFragment.TAG, "loadApplications 실행됨(1)-${params[0]}")
         val doc: Document = Jsoup.connect("${params[0]}").timeout(1000 * 100).get()
         val word = doc.select("span.txt_emph1").eq(1).text()
-        Log.d(SearchFragment.TAG,"뜻-meaning ${word}")
+        Log.d(SearchFragment.TAG, "뜻-meaning ${word}")
+        val k : Elements? = doc.select("ul.list_search li span.num_search")
+        val a = k?.size
+        Log.d("kk","${k}")
+        Log.d("aa","$a")
         var i = 0 //for문에 사용할 변수
         var meaning = "" //반환값으로 사용할 변수
-        var real = true
-        while(real) {
-            val m = doc.select("ul.list_search li span.num_search").eq(i).text().replace(".","")
-            val m2 = doc.select("ul.list_search li span.num_search").eq(i+1).text().replace(".","")
-
-            if(m.toInt() > m2.toInt() ){
-                Log.d(SearchFragment.TAG,"해결하기2 ${m}/${m2}")
-                real = false
-            }
-            else{
-                Log.d(SearchFragment.TAG,"해결하기1 ${m}/${m2}")
-                i++
+        if (a != null) {
+            if(a != 1) {
+                for (j in 0..(a - 2)) {
+                    val m = doc.select("ul.list_search li span.num_search").eq(j).text().replace(".", "")
+                    val m2 = doc.select("ul.list_search li span.num_search").eq(j + 1).text().replace(".", "")
+                    if (m.toInt() >= m2.toInt()) {
+                        Log.d(SearchFragment.TAG, "해결하기2 ${m}/${m2}")
+                        break
+                    } else {
+                        Log.d(SearchFragment.TAG, "해결하기1 ${m}/${m2}")
+                        i++
+                    }
+                }
             }
         }
-        Log.d(SearchFragment.TAG,"i의 값은? ${i}")
+        else{
+            meaning="뜻이 없습니다."
+        }
+        Log.d(SearchFragment.TAG, "i의 값은? ${i}")
         for (k in 0..i){
             meaning += doc.select("ul.list_search li span.num_search").eq(k).text()
             meaning += doc.select("ul.list_search li span.txt_search").eq(k).text()
             meaning += " "
-            Log.d(SearchFragment.TAG,"단어뜻-word ${meaning}")
+            Log.d(SearchFragment.TAG, "단어뜻-word ${meaning}")
         }
         //select부분 문법 찾아서 고치기
 
@@ -41,10 +51,10 @@ class AsyncTaskExample(private var fragment: SearchFragment) : AsyncTask<String,
     }
 
     override fun onPostExecute(result: String?) {
-        Log.d(SearchFragment.TAG,"loadApplications 실행됨(2)")
+        Log.d(SearchFragment.TAG, "loadApplications 실행됨(2)")
         super.onPostExecute(result)
         //Log.d(SearchFragment.TAG, "$result")
-        Log.d(SearchFragment.TAG,"단어뜻-word ${result}")
+        Log.d(SearchFragment.TAG, "단어뜻-word ${result}")
 
     }
 }
